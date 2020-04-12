b34e6a4743f95f84b7775ee22fc23186b257d778
BRUNO <5435873+jmbruno@users.noreply.github.com>
2020-04-12 15:25:07 +0200

    Add new functions

--- mpu6050.cpp
+++ mpu6050.cpp
@@ -1,30 +1,57 @@
-/* V0.0.1
+/* V0.0.2
  * RTC utilitaires Jean Marc BRUNO / MaYIoT
  * du 06/04/2020
+ * For Wire library v1.0.1+
  * *************************************************************************************/
 
 #include <mpu6050.hpp>
 
+uint8_t MPUadress = 0;
+int16_t dataMPU[32];
 int16_t aX = 0, aY = 0, aZ = 0;
 int16_t gX = 0, gY = 0, gZ = 0;
 int16_t temp = 0;
 //=======================================================================================
 
-//
-//---------------------------------------------------------------------------------------
-bool mpu6050::startMPU(uint8_t adress, uint8_t sda, uint8_t scl, uint32_t clock, bool beg) {
+/**
+ * Reset the MPU and configure the gyroscope to +- 2000 degrees/second and the accelerometer to +-16g.
+ * The low pass filter settings control how sensitive the sensors are to quick changes.
+ * In order of increasing sensitivity: 6, 5, 4, 3, 2, 1, 0, 7
+ * Info from MPU-9250 register map document.
+ * -gyro_lpf Gyroscope low pass filter setting,
+ *   7: 8kHz sampling rate, 36001Hz bandwidth, 0.17ms delay.
+ *   0: 8kHz sampling rate, 250Hz bandwidth, 0.97ms delay.
+ *   1: 1kHz sampling rate, 184Hz bandwidth, 2.9ms delay.
+ *   2: 1kHz sampling rate, 92Hz bandwidth, 3.9ms delay.
+ *   3: 1kHz sampling rate, 41Hz bandwidth, 5.9ms delay.
+ *   4: 1kHz sampling rate, 20Hz bandwidth, 9.9ms delay.
+ *   5: 1kHz sampling rate, 10Hz bandwidth, 17.85ms delay.
+ *   6: 1kHz sampling rate, 5Hz bandwidth, 33.48ms delay.
+ * -accel_lpf Accelerometer low pass filter setting,
+ *   7: 1kHz sampling rate, 420Hz 3dB bandwidth, 1.38ms delay.
+ *   0: 1kHz sampling rate, 218.1Hz 3dB bandwidth, 1.88ms delay.
+ *   1: 1kHz sampling rate, 218.1Hz 3dB bandwidth, 1.88ms delay.
+ *   2: 1kHz sampling rate, 99Hz 3dB bandwidth, 2.88ms delay.
+ *   3: 1kHz sampling rate, 44.8Hz 3dB bandwidth, 4.88ms delay.
+ *   4: 1kHz sampling rate, 21.2Hz 3dB bandwidth, 8.87ms delay.
+ *   5: 1kHz sampling rate, 10.2Hz 3dB bandwidth, 16.83ms delay.
+ *   6: 1kHz sampling rate, 5.05Hz 3dB bandwidth, 32.48ms delay.
+ *-------------------------------------------------------------------------------------*/
+bool mpu6050::startMPU(uint8_t I2Cadr, uint8_t sda, uint8_t scl, uint32_t clock, bool beg) {
   bool status = true;
 
   if (beg) status = Wire.begin(sda, scl, clock);
-  Wire.beginTransmission(MPU_ADR);
+  Wire.beginTransmission(I2Cadr);
   Wire.write(PWR_MGMT_1);
   Wire.write(0);
   if (Wire.endTransmission(true) == 0) status = true; else status = false;
 
   if (status) {
 #if _DUSB>= 1
-    Serial.println("MPU6050 device Initialized...");
+    Serial.printf("MPU6050 device Initialized at 0x%02X Adress...\n", I2Cadr);
 #endif
+    MPUadress = I2Cadr;
+
     return true;
   } else {
 #if _DUSB>= 1
@@ -35,6 +62,193 @@ bool mpu6050::startMPU(uint8_t adress, uint8_t sda, uint8_t scl, uint32_t clock,
 }
 //---------------------------------------------------------------------------------------
 
+//
+//---------------------------------------------------------------------------------------
+String mpu6050::findMPU() {
+  int8_t sensorId;
+
+  sensorId = readByte(WHO_AM_I);
+#if _DUSB>=1
+  Serial.printf("***** MPU Id: 0x%02x\n", sensorId);
+#endif
+
+  switch(sensorId) {
+    case MPU_6050_ID: return "MPU-6050";
+    case MPU_6500_ID: return "MPU-6500";
+    case MPU_9250_ID: return "MPU-9250";
+    case MPU_9255_ID: return "MPU-9255";
+    default:          return "--------";
+  }
+}
+//--------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+uint8_t mpu6050::writeByte(uint8_t regAdr, uint8_t data) {
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  Wire.write(data);
+
+  return Wire.endTransmission();
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+uint8_t mpu6050::writeWord(uint8_t regAdr, int16_t data) {
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+
+  return Wire.endTransmission();
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+uint8_t mpu6050::writeBytes(uint8_t regAdr, uint8_t length, int8_t *data) {
+  uint8_t status = 0;
+
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  if (length != 0) {
+    for (uint8_t i= 0; i < length; i++) {
+      Wire.write(data[i]);
+    }
+  }
+  status = Wire.endTransmission();
+
+  return status == 0;
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+uint8_t mpu6050::readBytes(uint8_t regAdr, uint8_t length, int8_t *data, uint16_t timeOut) {
+  uint8_t status = 0, count = 0;
+  uint32_t t1 = millis();
+
+#if _DUSB>=2
+  Serial.printf("I2C (0x%0x) reading %u bytes from 0x%02X...\n", I2Caddr, length, regAddr);
+#endif
+
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  status = Wire.endTransmission();
+
+  for (uint8_t k= 0; k < length; k += min(length, BUF_LENGTH)) {
+    //status = writeByte(regAddr, 0);
+    if (status != 0) return status;
+
+    Wire.beginTransmission(MPUadress);
+    Wire.requestFrom(MPUadress, (uint8_t)__min(length, BUF_LENGTH));
+
+    for (; Wire.available() && (timeOut == 0 || millis() - t1 < timeOut); count++) {
+      data[count] = Wire.read();
+#if _DUSB>=1
+      Serial.printf("%02X", data[count]);
+      if ((count + 1) < length) Serial.print(" ");
+#endif
+    }
+    Wire.endTransmission();
+  }
+
+  if (timeOut > 0 && millis() - t1 >= timeOut && count < length) count = -1; // timeout
+
+  return count;
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+int8_t mpu6050::readByte(uint8_t regAdr) {
+  int8_t status = 0;
+  int8_t data = 0;
+  //uint32_t t1 = millis();
+
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  status = Wire.endTransmission();
+  if (status != 0) return -1;
+
+  Wire.requestFrom(MPUadress, (uint8_t)1);
+
+  data = Wire.read();
+
+#if _DUSB>=2
+  Serial.printf("%02X", data);
+#endif
+
+   Wire.endTransmission();
+
+  return data;
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+int16_t mpu6050::readWord(uint8_t regAdr) {
+  uint8_t status = 0;
+  int16_t data = 0;
+  //uint32_t t1 = millis();
+
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  status = Wire.endTransmission();
+  if (status != 0) return -1;
+
+  Wire.requestFrom(MPUadress, (uint8_t)2);
+
+  data = Wire.read() << 8;
+  data = data | Wire.read();
+
+#if _DUSB>=2
+  Serial.printf("%04X", data);
+#endif
+
+   Wire.endTransmission();
+
+  return data;
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+uint8_t mpu6050::readWords(uint8_t regAdr, uint8_t length, int16_t *data, uint16_t timeOut) {
+  bool msb = true;
+  int8_t status = 0, count = 0;
+  uint32_t t1 = millis();
+
+  Wire.beginTransmission(MPUadress);
+  Wire.write(regAdr);
+  status = Wire.endTransmission();
+
+  for (uint8_t k= 0; k < length * 2; k += __min(length * 2, BUF_LENGTH)) {
+    Wire.requestFrom(MPUadress, (uint8_t)__min(length * 2, BUF_LENGTH));
+
+    for (; Wire.available() && count < length && (timeOut == 0 || millis() - t1 < timeOut); ) {
+      if (msb) {
+        data[count] = Wire.read() << 8;
+        count++;
+      } else {
+        data[count] = Wire.read();
+        count++;
+      }
+      msb = !msb;
+#if _DUSB>=2
+      Serial.printf("%04X", data[count]);
+      if (count + 1 < length) Serial.print(" ");
+#endif
+    }
+    Wire.endTransmission();
+  }
+
+  if (timeOut > 0 && millis() - t1 >= timeOut && count < length) status = -1; else status = count; // timeout
+
+  return status;
+}
+//---------------------------------------------------------------------------------------
+
+
 // Read MPU6050 Registers
 // Parameters:
 // - start: start Register
@@ -42,62 +256,20 @@ bool mpu6050::startMPU(uint8_t adress, uint8_t sda, uint8_t scl, uint32_t clock,
 // Return:  true if good
 //---------------------------------------------------------------------------------------
 bool mpu6050::readMPU(uint8_t start, uint8_t nr) {
-  uint16_t aXl = 0, aYl = 0, aZl = 0, tempL = 0;
-  uint16_t aXh = 0, aYh = 0, aZh = 0, tempH = 0;
-  uint16_t gXl = 0, gYl = 0, gZl = 0;
-  uint16_t gXh = 0, gYh = 0, gZh = 0;
-  uint8_t read = start + nr;
-  bool status = false;
+  uint8_t status = false;
 
-  Wire.beginTransmission(MPU_ADR);
-  Wire.write(ACCEL_XOUT_H);
-  if (Wire.endTransmission(false) == 0) status = true; else return false;
-  Wire.requestFrom(MPU_ADR, 14, 1);  // request a total of nr registers
+  //status = writeByte(MPUadress, ACCEL_XOUT_H);
 
-  if (nr < 1 || nr > 14) {
+  if (nr < 1 || nr > 7) {
 #if _DUSB>=1
-    Serial.println("Number Register not good");
+    Serial.println("Number Register to read not good");
     status = false;
 #endif
 
-    return false;
+    return status;
   }
 
-  switch (start) {
-    case ACCEL_XOUT_H: aXh  = Wire.read(); if (read < ACCEL_XOUT_L) break;  // (ACCEL_XOUT_H)
-    case ACCEL_XOUT_L: aXl  = Wire.read(); if (read < ACCEL_YOUT_H) break;  // (ACCEL_XOUT_L)
-    case ACCEL_YOUT_H: aYh  = Wire.read(); if (read < ACCEL_YOUT_L) break;  // (ACCEL_YOUT_H)
-    case ACCEL_YOUT_L: aYl  = Wire.read(); if (read < ACCEL_ZOUT_H) break;  // (ACCEL_YOUT_L)
-    case ACCEL_ZOUT_H: aZh  = Wire.read(); if (read < ACCEL_ZOUT_L) break;  // (ACCEL_ZOUT_H)
-    case ACCEL_ZOUT_L: aZl  = Wire.read(); if (read < TEMP_OUT_H)   break;  // (ACCEL_ZOUT_L)
-    case TEMP_OUT_H  : tempH= Wire.read(); if (read < TEMP_OUT_L)   break;  // (TEMP_OUT_H)
-    case TEMP_OUT_L  : tempL= Wire.read(); if (read < GYRO_XOUT_H)  break;  // (TEMP_OUT_L)
-    case GYRO_XOUT_H : gXh  = Wire.read(); if (read < GYRO_XOUT_L)  break;  // (GYRO_XOUT_H)
-    case GYRO_XOUT_L : gXl  = Wire.read(); if (read < GYRO_YOUT_H)  break;  // (GYRO_XOUT_L)
-    case GYRO_YOUT_H : gYh  = Wire.read(); if (read < GYRO_YOUT_L)  break;  // (GYRO_YOUT_H)
-    case GYRO_YOUT_L : gYl  = Wire.read(); if (read < GYRO_ZOUT_H)  break;  // (GYRO_YOUT_L)
-    case GYRO_ZOUT_H : gZh  = Wire.read(); if (read < GYRO_ZOUT_L)  break;  // (GYRO_ZOUT_H)
-    case GYRO_ZOUT_L : gZl  = Wire.read(); break;                           // (GYRO_ZOUT_L)
-    default:
-#if _DUSB>=1
-      Serial.println("Adress not defined");
-#endif
-      return false;
-  }
-
-  aX = aXh << 8 | aXl;
-  aY = aYh >> 8 | aYl;
-  aY = aZh >> 8 | aZl;
-  temp = tempH >> 8 | tempL;
-  gX = gXh >> 8 | gXl;
-  gY = gYh >> 8 | gYl;
-  gZ = gZh >> 8 | gZl;
-
-#if _DUSB>=1
-  Serial.printf("- AcX = %d | AcY = %d | AcZ = %d\n", aX, aY, aZ);
-  Serial.printf("- GyX = %d | GyY = %d | GuZ = %d\n", gX, gY, gZ);
-  Serial.printf("- Temp= %3.1f °C\n", float(temp) / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
-#endif
+  readWords(ACCEL_XOUT_H, 7, dataMPU, (uint16_t)1000);
 
   return status;
 }
@@ -165,5 +337,49 @@ void AcceleroCalibre(uint16_t calTime) {
 }
 //---------------------------------------------------------------------------------------*/
 
+//
+//---------------------------------------------------------------------------------------
+void mpu6050::setScaleAccel(uint8_t range) {
+  writeByte(ACCEL_CONFIG, range);
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+void mpu6050::setScaleGyros(uint8_t range) {
+  writeByte(GYRO_CONFIG, range);
+}
+//---------------------------------------------------------------------------------------
+
+//
+//---------------------------------------------------------------------------------------
+boolean mpu6050::computeGyroOffset(uint8_t moy) {
+  uint16_t OfgX = 0, OfgY = 0, OfgZ = 0;
+
+  writeWord(GX_OFFSET_H, 0);
+  writeWord(GY_OFFSET_H, 0);
+  writeWord(GZ_OFFSET_H, 0);
+
+  for (uint8_t i= 0; i < moy; i++) {
+    OfgX += readWord(GYRO_XOUT_H);
+    OfgY += readWord(GYRO_YOUT_H);
+    OfgZ += readWord(GYRO_ZOUT_H);
+    delay(10);
+  }
+
+  OfgX = OfgX / moy;
+  OfgY = OfgY / moy;
+  OfgZ = OfgZ / moy;
+  if (OfgX > MAX_VAL || OfgY > MAX_VAL || OfgZ > MAX_VAL) {
+    return false;
+  }
+
+  writeWord(GX_OFFSET_H, -2 * OfgX);
+  writeWord(GY_OFFSET_H, -2 * OfgY);
+  writeWord(GZ_OFFSET_H, -2 * OfgZ);
+
+  return true;
+}
+//---------------------------------------------------------------------------------------
 mpu6050 mpu;
 //=======================================================================================
--- mpu6050.hpp
+++ mpu6050.hpp
@@ -3,39 +3,80 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+
+#define __min(a,b) ((a)<(b)?(a):(b))
 //=======================================================================================
 
-const uint8_t MPU_ADR      = 0x68;      // Adresse du MPU6050
-const uint8_t PWR_MGMT_1   = 0x6B;      // PWR_MGMT_1 register
-const uint8_t ACCEL_XOUT_H = 0x3B;
-const uint8_t ACCEL_XOUT_L = 0x3C;
-const uint8_t ACCEL_YOUT_H = 0x3D;
-const uint8_t ACCEL_YOUT_L = 0x3E;
-const uint8_t ACCEL_ZOUT_H = 0x3F;
-const uint8_t ACCEL_ZOUT_L = 0x40;
-const uint8_t TEMP_OUT_H   = 0x41;
-const uint8_t TEMP_OUT_L   = 0x42;
-const uint8_t GYRO_XOUT_H  = 0x43;
-const uint8_t GYRO_XOUT_L  = 0x44;
-const uint8_t GYRO_YOUT_H  = 0x45;
-const uint8_t GYRO_YOUT_L  = 0x46;
-const uint8_t GYRO_ZOUT_L  = 0x47;
-const uint8_t GYRO_ZOUT_H  = 0x48;
+const uint8_t MPU_6050_ADR      = 0x68;      // Adresse du MPU6050
+const uint8_t MPU_6050_ID       = 0x68;
+const uint8_t MPU_6500_ID       = 0x70;
+const uint8_t MPU_9250_ID       = 0x71;
+const uint8_t MPU_9255_ID       = 0x73;
+const uint8_t SIGNAL_PATH_RESET = 0x68;
+const uint8_t REG_USER_CTRL     = 0x6a;
+const uint8_t PWR_MGMT_1        = 0x6B;      // PWR_MGMT_1 register
+const uint8_t WHO_AM_I          = 0x75;
+const uint8_t ACCEL_XOUT_H      = 0x3B;
+const uint8_t ACCEL_XOUT_L      = 0x3C;
+const uint8_t ACCEL_YOUT_H      = 0x3D;
+const uint8_t ACCEL_YOUT_L      = 0x3E;
+const uint8_t ACCEL_ZOUT_H      = 0x3F;
+const uint8_t ACCEL_ZOUT_L      = 0x40;
+const uint8_t TEMP_OUT_H        = 0x41;
+const uint8_t TEMP_OUT_L        = 0x42;
+const uint8_t GYRO_XOUT_H       = 0x43;
+const uint8_t GYRO_XOUT_L       = 0x44;
+const uint8_t GYRO_YOUT_H       = 0x45;
+const uint8_t GYRO_YOUT_L       = 0x46;
+const uint8_t GYRO_ZOUT_L       = 0x47;
+const uint8_t GYRO_ZOUT_H       = 0x48;
+const uint8_t CONFIG            = 0x1A;
+const uint8_t GYRO_CONFIG       = 0x1B;
+const uint8_t ACCEL_CONFIG      = 0x1C;
+const uint8_t ACCEL_CONFIG2     = 0x1D;
+const uint8_t GX_OFFSET_H       = 0x13;      // Offset X Gyroscope
+const uint8_t GY_OFFSET_H       = 0x15;      // Offset Y Gyroscope
+const uint8_t GZ_OFFSET_H       = 0x17;      // Offset Z Gyroscope
+const uint8_t GFS_SEL_250       = 0;         // Calibre +-250°/s Gyrosope
+const uint8_t GFS_SEL_500       = 1;         // Calibre +-500°/s Gyrosope
+const uint8_t GFS_SEL_1000      = 2;         // Calibre +-1000°/s Gyrosope
+const uint8_t GFS_SEL_2000      = 3;         // Calibre +-2000°/s Gyrosope
+const uint8_t AFS_SEL_2         = 0;         // Calibre +-2g Accelero
+const uint8_t AFS_SEL_4         = 1;         // Calibre +-4g Accelero
+const uint8_t AFS_SEL_8         = 2;         // Calibre +-8g Accelero
+const uint8_t AFS_SEL_16        = 3;         // Calibre +-16g Accelero
+const uint8_t BUF_LENGTH        = 32;
+const uint16_t MAX_VAL          = 60;        // stabilite de l'offset
+const uint16_t MAX_CAL          = 65535 /2;  // Value of max. register 
 
 extern int16_t aX, aY, aZ;
 extern int16_t gX, gY, gZ;
 extern int16_t temp;
-
+extern int16_t dataMPU[32];
+extern uint8_t MPUadress;
 //---------------------------------------------------------------------------------------
+
 class mpu6050 {
 
-  public :
+  public:
 
-    bool startMPU(uint8_t adress, uint8_t sda, uint8_t scl, uint32_t clock, bool beg);
+    bool startMPU(uint8_t I2Cadr, uint8_t sda, uint8_t scl, uint32_t clock, bool beg);
+    String findMPU();
+    int16_t readWord(uint8_t regAdr);
     bool readMPU(uint8_t start, uint8_t nr);
+    void setScaleAccel(uint8_t range);
+    void setScaleGyros(uint8_t range);
+    boolean computeGyroOffset(uint8_t moy);
 
   private:
-
+    int8_t  readByte(uint8_t regAdr);
+    uint8_t readBytes(uint8_t regAdr, uint8_t length, int8_t  *data, uint16_t timeOut);
+    uint8_t readWords(uint8_t regAdr, uint8_t length, int16_t *data, uint16_t timeOut);
+    uint8_t writeByte(uint8_t regAdr, uint8_t data);
+    uint8_t writeWord(uint8_t regAdr, int16_t data);
+    uint8_t writeBytes(uint8_t regAdr, uint8_t length, int8_t *data);
+    boolean readRegister(uint8_t adress, uint8_t regist);
+    boolean writeRegister(uint8_t adress, uint8_t regist);
 };
 
 extern mpu6050 mpu;
